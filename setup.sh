3a8eb873288a9675f86df025b2185adb12eb28a9
Danny <drench+github@gmail.com>
2011-04-15 09:14:04 -0500

    Make this a Drupal 6 server

--- setup.sh
+++ setup.sh
@@ -5,11 +5,48 @@ export DEBIAN_FRONTEND
 
 apt-get --yes update
 
-for X in libapache2-mod-php5 php5-mysql mysql-server; do
+for X in libapache2-mod-php5 php5-mysql php5-gd mysql-server; do
 	apt-get --yes install $X
 done
 
-echo '<?php phpinfo(); ?>' > /var/www/index.php
+cat <<MYSEEQUAL | mysql -u root
+CREATE DATABASE drupal;
+GRANT ALL ON drupal.* TO drupal@localhost IDENTIFIED BY '';
+MYSEEQUAL
+
+ownerid=`stat -c%u /vagrant`
+
+adduser --no-create-home \
+    --uid $ownerid \
+    --ingroup www-data \
+    --disabled-password \
+    --disabled-login \
+    --gecos "LAMP user" \
+    lamp
+
+echo "export APACHE_RUN_USER=lamp" >> /etc/apache2/envvars
+
+a2enmod rewrite
+
+cat > /etc/apache2/conf.d/drupal <<YUKKS
+<Directory /var/www>
+    AllowOverride all
+    Include /var/www/.htaccess
+</Directory>
+YUKKS
+
+cd /vagrant && \
+wget --timestamping \
+    http://ftp.drupal.org/files/projects/drupal-6.20.tar.gz && \
+sudo -u\#$ownerid tar zvxf drupal-6.20.tar.gz
+
 rm -v /var/www/index.html
+rmdir /var/www
+ln -s /vagrant/drupal-6.20 /var/www
+
+cd /var/www/sites/default && \
+cp -v default.settings.php settings.php && \
+patch < /vagrant/settings.patch
+chmod 0444 settings.php
 
 /etc/init.d/apache2 restart
