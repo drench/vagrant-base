8854d9c37813d70189be344528de46fa494a6836
Danny <drench+github@gmail.com>
2011-04-14 19:42:48 -0500

    Generic 'base' branch. No packages or services.

--- setup.sh
+++ setup.sh
@@ -4,12 +4,3 @@ DEBIAN_FRONTEND=noninteractive
 export DEBIAN_FRONTEND
 
 apt-get --yes update
-
-for X in libapache2-mod-php5 php5-mysql mysql-server; do
-	apt-get --yes install $X
-done
-
-echo '<?php phpinfo(); ?>' > /var/www/index.php
-rm -v /var/www/index.html
-
-/etc/init.d/apache2 restart
