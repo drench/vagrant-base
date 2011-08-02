b05ac43d1177ed29b05c8c02c2c5741df3f4e0d2
Danny Rench <drench+github@gmail.com>
2011-08-02 13:30:16 -0500

    upgrade to WordPress 3.1.3

--- setup.sh
+++ setup.sh
@@ -36,8 +36,8 @@ YUKKS
 
 cd /vagrant && \
 wget --timestamping \
-    http://wordpress.org/wordpress-3.1.2.tar.gz && \
-sudo -u\#$ownerid tar zvxf wordpress-3.1.2.tar.gz
+    http://wordpress.org/wordpress-3.1.3.tar.gz && \
+sudo -u\#$ownerid tar zvxf wordpress-3.1.3.tar.gz
 
 rm -v /var/www/index.html
 rmdir /var/www
