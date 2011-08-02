8611d7db74276cfacdbd558bd0a13ff629c3801c
Danny Rench <drench+github@gmail.com>
2011-08-02 14:22:08 -0500

    upgrade to Drupal 7.1

--- setup.sh
+++ setup.sh
@@ -37,12 +37,12 @@ YUKKS
 
 cd /vagrant && \
 wget --timestamping \
-    http://ftp.drupal.org/files/projects/drupal-7.0.tar.gz && \
-sudo -u\#$ownerid tar zvxf drupal-7.0.tar.gz
+    http://ftp.drupal.org/files/projects/drupal-7.1.tar.gz && \
+sudo -u\#$ownerid tar zvxf drupal-7.1.tar.gz
 
 rm -v /var/www/index.html
 rmdir /var/www
-ln -s /vagrant/drupal-7.0 /var/www
+ln -s /vagrant/drupal-7.1 /var/www
 
 cd /var/www/sites/default && \
 cp -v default.settings.php settings.php && \
